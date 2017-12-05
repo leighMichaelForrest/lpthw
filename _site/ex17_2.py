27a952303e4960131f578d70c7233150c6ee64ed
Leigh_Code <leigh_code@Leighs-MBP.fios-router.home>
2017-12-05 18:30:19 -0500

    game files added to monster_maze

--- _site/ex17_2.py
+++
@@ -1,5 +0,0 @@
-from sys import argv
-
-script, from_file, to_file = argv
-
-with open(from_file) as f: indata = f.read(); with open(to_file, 'w') as f: f.write(indata)
--- _site/mystuff.py
+++
@@ -1,7 +0,0 @@
-class MyStuff(object):
-
-    def __init__(self):
-        self.tangerine = "And now a thousand years between"
-
-    def apple(self):
-        print("I AM CLASSY APPLES!")
